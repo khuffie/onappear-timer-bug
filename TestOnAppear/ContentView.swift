a7d11404a5f9369f1ea9d8fa45ff3470594d7f5d
Ahmed El-Khuffash <ahmed@el-khuffash.com>
2020-07-04 17:36:52 -0400

    Initial Commit

---
+++ TestOnAppear/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestOnAppear
+//
+//  Created by Ahmed El-Khuffash on 2020-07-04.
+//  Copyright © 2020 Ahmed El-Khuffash. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
